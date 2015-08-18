b67b9753845a4eae3f097d46a39789b1482e2f54
Naveen <dexter2305@gmail.com>
2015-08-19 00:14:37 +0530

    Added partial view for list candidates & add candidate.

--- src/main/webapp/app/js/app.js
+++ src/main/webapp/app/js/app.js
@@ -1,24 +1,37 @@
-angular.module('PromatApp', ['apiService'])
+angular.module('PromatApp', ['apiService','ngRoute'])
 
-.controller('candidatesController', function ($scope, Candidates) {
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+      when('/addCandidate', {
+        templateUrl: 'app/views/add-candidate.html',
+        controller: 'AddCandidateController'
+      }).
+      when('/showCandidates', {
+        templateUrl: 'app/views/show-candidates.html',
+        controller: 'ShowCandidatesController'
+      }).
+      otherwise({
+        redirectTo: '/showCandidates'
+      });
+  }])
+.controller('AddCandidateController', function($scope){
+    $scope.message = 'Add candidate use case';
+})
+.controller('ShowCandidatesController', function($scope, Candidates){
+  	this.init = function () {
+  	    console.log("getting all candidates");
+  		$scope.candidates = Candidates.query();
+  		//this.getAllCandidates();
+  		$scope.message = "show candidate use case !! "
+  		console.log("invoked init()");
+  	};
 
-	//$scope.candidates = [];
 
+  	this.getAllCandidates = function() {
+  		$scope.candidates = apiService.query();
+  		console.log("invoked getAllCandidates()");
+  	};
+  	this.init();
 
-
-	this.init = function () {
-	    console.log("getting all candidates");
-		$scope.candidates = Candidates.query();
-		//this.getAllCandidates();
-		console.log("invoked init()");
-	};
-
-
-	this.getAllCandidates = function() {
-		$scope.candidates = apiService.query();
-		console.log("invoked getAllCandidates()");
-	};
-
-
-	this.init();
-});
+})
+;
