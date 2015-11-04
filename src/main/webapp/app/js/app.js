583e874a42409e96d4d7f4092ae66ba331b0e098
Naveen <dexter2305@gmail.com>
2015-11-04 08:56:54 +0530

    New layout for UserProfile - work in progress

--- src/main/webapp/app/js/app.js
+++ src/main/webapp/app/js/app.js
@@ -1,104 +1,108 @@
-angular.module('PromatApp', ['apiService','ngRoute'])
+angular.module('PromatApp', ['apiService', 'ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-    .when('/addCandidate', {
-        templateUrl: 'app/views/candidate-profile.html',
-        controller: 'AddCandidateController'
-    })
-    .when('/candidates', {
-        templateUrl: 'app/views/show-candidates.html',
-        controller: 'ShowCandidatesController'
-    })
-    .when('/candidate/:candidateId', {
-            templateUrl: 'app/views/candidate-profile.html',
-            controller: 'ShowCandidateDetailsController'
-    })
-    .when('/home', {
-              templateUrl: 'app/views/home-view.html',
-              controller: 'HomeViewController'
-    })
-    .otherwise({
-        redirectTo: '/home'
-    });
+.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider
+			.when('/addCandidate', {
+				templateUrl: 'app/views/candidate-profile.html',
+				controller: 'AddCandidateController'
+			})
+			.when('/candidates', {
+				templateUrl: 'app/views/show-candidates.html',
+				controller: 'ShowCandidatesController'
+			})
+			.when('/candidate/:candidateId', {
+				templateUrl: 'app/views/candidate-profile-sp.html',
+				controller: 'ShowCandidateDetailsController'
+			})
+			.when('/home', {
+				templateUrl: 'app/views/home-view.html',
+				controller: 'HomeViewController'
+			})
+			.otherwise({
+				redirectTo: '/home'
+			});
   }])
-.controller('AddCandidateController', function($scope, Candidates){
-    $scope.addCandidate = function(candidate){
-        Candidates.save(candidate);
-        $scope.message = candidate.name + " created successfully.";
+	.controller('AddCandidateController', function ($scope, Candidates) {
+		$scope.addCandidate = function (candidate) {
+			Candidates.save(candidate);
+			$scope.message = candidate.name + " created successfully.";
 
-    };
+		};
 
-    $scope.reset = function(){
-        $scope.candidate = new Object();
-    };
-    $scope.isNewProfile = true;
-})
-.controller('ShowCandidatesController', function($scope, Candidates){
-  	this.init = function () {
-  		this.getAllCandidates();
-  	};
-  	this.getAllCandidates = function() {
-  		$scope.candidates = Candidates.query();
-  	};
-    $scope.deleteCandidate = function(candidate){
-        candidate.$delete(function(){
-            $scope.candidates = Candidates.query();
-        });
-    };
-  	this.init();
-})
-.controller('ShowCandidateDetailsController', function($scope, $routeParams, Candidates){
-    this.init = function(candidateId){
-        Candidates.get({id:candidateId},function(candidate){
-            $scope.candidate = candidate;
-        });
-    };
+		$scope.reset = function () {
+			$scope.candidate = new Object();
+		};
+		$scope.isNewProfile = true;
+	})
+	.controller('ShowCandidatesController', function ($scope, Candidates) {
+		this.init = function () {
+			this.getAllCandidates();
+		};
+		this.getAllCandidates = function () {
+			$scope.candidates = Candidates.query();
+		};
+		$scope.deleteCandidate = function (candidate) {
+			candidate.$delete(function () {
+				$scope.candidates = Candidates.query();
+			});
+		};
+		this.init();
+	})
+	.controller('ShowCandidateDetailsController', function ($scope, $routeParams, Candidates) {
+		this.init = function (candidateId) {
+			Candidates.get({
+				id: candidateId
+			}, function (candidate) {
+				$scope.candidate = candidate;
+			});
+		};
 
-    this.init($routeParams.candidateId);
+		this.init($routeParams.candidateId);
 
-    $scope.updateCandidate = function(candidate){
-        candidate.$update();
-        $scope.message = candidate.name + " updated successfully.";
-    };
+		$scope.updateCandidate = function (candidate) {
+			candidate.$update();
+			$scope.message = candidate.name + " updated successfully.";
+		};
 
-    $scope.deleteEducation = function(){
-        console.log("delete education invoked for some education ");
-    };
+		$scope.addNewEducation = function () {
+			console.log("Add new education invoked... ");
+		};
+		$scope.removeThisEducation = function () {
+			console.log("delete education invoked... ");
+		};
+	})
+	.controller('HomeViewController', function () {
 
-})
-.controller('HomeViewController', function(){
-
-})
-.controller('TabNavigationController', function($scope){
-        // Init tab index
-        this.tab = 0;
-        // setting tab index
-        this.setTab = function (tabIdx) {
-            this.tab = tabIdx;
-        };
-        // checking tab index
-        this.isTab = function (tabIdx) {
-            return this.tab === tabIdx;
-        };
-})
-.controller('SkinToneOptionsController',function($scope){
-    $scope.skinTones = [
+	})
+	.controller('TabNavigationController', function ($scope) {
+		// Init tab index
+		this.tab = 0;
+		// setting tab index
+		this.setTab = function (tabIdx) {
+			this.tab = tabIdx;
+		};
+		// checking tab index
+		this.isTab = function (tabIdx) {
+			return this.tab === tabIdx;
+		};
+	})
+	.controller('SkinToneOptionsController', function ($scope) {
+		$scope.skinTones = [
         "DARK",
         "MODERATE",
         "FAIR"
     ];
-})
-.controller('BodyTypeOptionsController',function($scope){
-    $scope.bodyTypes = [
+	})
+	.controller('BodyTypeOptionsController', function ($scope) {
+		$scope.bodyTypes = [
         "NORMAL",
         "SLIM",
         "ATHLETIC",
         "HEAVY"
     ];
-})
-.controller('BloodGroupOptionsController', function($scope){
-    $scope.bloodGroups = [
+	})
+	.controller('BloodGroupOptionsController', function ($scope) {
+		$scope.bloodGroups = [
         "OPOSITIVE",
         "ONEGATIVE",
         "APOSITIVE",
@@ -108,5 +112,4 @@ angular.module('PromatApp', ['apiService','ngRoute'])
         "ABPOSITIVE",
         "ABNEGATIVE"
     ];
-})
-;
+	});
